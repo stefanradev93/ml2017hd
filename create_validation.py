ab7d02371a3cd3feb89160b165d386a7503f4b30
Gloria Feher <feher@stud.uni-heidelberg.de>
2018-03-20 18:44:59 +0100

    Save ratings as float, not rounded to int.

--- create_validation.py
+++ create_validation.py
@@ -8,8 +8,8 @@ import numpy as np
 
 img_path = '../project/all_females_combined'
 rating_path = './ratings_s_combined.txt'
-save_path_val = '../project/all_females_combined_val_s'
-save_path_train = '../project/all_females_combined_train_s'
+save_path_val = '../project/eval_data/all_females_combined_val_s'
+save_path_train = '../project/eval_data/all_females_combined_train_s'
 maxNum = 7
 minNum = 1
 
@@ -20,16 +20,16 @@ if not os.path.isdir(save_path_train):
     os.makedirs(save_path_train)
 
 orig_ratings = np.genfromtxt(rating_path)
-orig_ratings = np.round(orig_ratings, 0).astype(np.int8)
+rounded_ratings = np.round(orig_ratings, 0).astype(np.int8)
 
 # Move all images according to ratings
 orig_images = sorted(os.listdir(img_path))
-assert len(orig_ratings) == len(orig_images)
+assert len(rounded_ratings) == len(orig_images)
 
-perm_ratings = np.random.permutation(len(orig_ratings))
+perm_ratings = np.random.permutation(len(rounded_ratings))
 
 images = np.array(orig_images)[perm_ratings]
-ratings = orig_ratings[perm_ratings]
+ratings = rounded_ratings[perm_ratings]
 
 np.unique(ratings)
 
@@ -57,23 +57,31 @@ for i,r in zip(images,ratings):
 
 text_file = open(save_path_val + "/val_ratings.txt", "w")
 #make sure all selected ratings are correct
+save_ratings = []
 for r,sub in enumerate(selected_img):
     for img in sub:
         idx = orig_images.index(img)
-        assert(orig_ratings[idx] == r)
+        assert(rounded_ratings[idx] == r)
         #move selected_img to separate folder & create corresponding val_ratings.txt
-        copyfile(img_path + "/" + img,save_path_val + "/" + str(r) + "_" + img) #copy rating into filename to ensure correct order
-        text_file.write(str(r) + "\n")
+        copyfile(img_path + "/" + img,save_path_val + "/" + str(orig_ratings[idx]) + "_" + img) #copy rating into filename to ensure correct order
+        save_ratings.append(str(orig_ratings[idx]))
+save_ratings.sort()
+for r in save_ratings:
+    text_file.write(r + "\n")
 text_file.close()
 
 #do the same for train set
 text_file = open(save_path_train + "/train_ratings.txt", "w")
 #make sure all selected ratings are correct
+save_ratings = []
 for r,sub in enumerate(train_img):
     for img in sub:
         idx = orig_images.index(img)
-        assert(orig_ratings[idx] == r)
+        assert(rounded_ratings[idx] == r)
         #move selected_img to separate folder & create corresponding train_ratings.txt
-        copyfile(img_path + "/" + img,save_path_train + "/"+ str(r) + "_" + img)
-        text_file.write(str(r) + "\n")
+        copyfile(img_path + "/" + img,save_path_train + "/"+ str(orig_ratings[idx]) + "_" + img)
+        save_ratings.append(str(orig_ratings[idx]))
+save_ratings.sort()
+for r in save_ratings:
+    text_file.write(r + "\n")
 text_file.close()
