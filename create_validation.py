c1c297e813c0b4c6e8aaf18f298b4c98473cd6fd
Gloria Feher <feher@stud.uni-heidelberg.de>
2018-03-21 23:02:12 +0100

    Update.

--- create_validation.py
+++ create_validation.py
@@ -1,20 +1,20 @@
 '''
-This script creates a validation split of maxNum instances per class (min. minNum if class is rare) using the image filepath and rating filepath.
+This script creates a test split of maxNum instances per class (min. minNum if class is rare) using the image filepath and rating filepath.
 '''
 
 from shutil import copyfile
 import os
 import numpy as np
 
-img_path = '../project/all_females_combined'
-rating_path = './ratings_s_combined.txt'
-save_path_val = '../project/eval_data/all_females_combined_val_s'
-save_path_train = '../project/eval_data/all_females_combined_train_s'
+img_path = '../project/all_males_combined'
+rating_path = './ratings_g_combined.txt'
+save_path_test = '../project/eval_data/all_males_combined_test_s'
+save_path_train = '../project/eval_data/all_males_combined_train_s'
 maxNum = 7
 minNum = 1
 
-if not os.path.isdir(save_path_val):
-    os.makedirs(save_path_val)
+if not os.path.isdir(save_path_test):
+    os.makedirs(save_path_test)
 
 if not os.path.isdir(save_path_train):
     os.makedirs(save_path_train)
@@ -55,15 +55,15 @@ for i,r in zip(images,ratings):
         #put image in train set
         train_img[r].append(i)
 
-text_file = open(save_path_val + "/val_ratings.txt", "w")
+text_file = open(save_path_test + "/test_ratings.txt", "w")
 #make sure all selected ratings are correct
 save_ratings = []
 for r,sub in enumerate(selected_img):
     for img in sub:
         idx = orig_images.index(img)
         assert(rounded_ratings[idx] == r)
-        #move selected_img to separate folder & create corresponding val_ratings.txt
-        copyfile(img_path + "/" + img,save_path_val + "/" + str(orig_ratings[idx]) + "_" + img) #copy rating into filename to ensure correct order
+        #move selected_img to separate folder & create corresponding test_ratings.txt
+        copyfile(img_path + "/" + img,save_path_test + "/" + str(orig_ratings[idx]) + "_" + img) #copy rating into filename to ensure correct order
         save_ratings.append(str(orig_ratings[idx]))
 save_ratings.sort()
 for r in save_ratings:
